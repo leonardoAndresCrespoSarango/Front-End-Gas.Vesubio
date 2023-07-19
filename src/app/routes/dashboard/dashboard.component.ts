265623a855b25748f581bbe688ff836dcb33e954
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-07-19 18:08:24 -0500

    commit

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,14 +1,5 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  OnDestroy,
-  ChangeDetectionStrategy,
-  NgZone,
-} from '@angular/core';
-import { SettingsService } from '@core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ChangeDetectionStrategy, NgZone } from '@angular/core';
 import { Subscription } from 'rxjs';
-
 import { DashboardService } from './dashboard.service';
 
 @Component({
@@ -18,51 +9,31 @@ import { DashboardService } from './dashboard.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [DashboardService],
 })
-export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = this.dashboardSrv.getData();
-
-  messages = this.dashboardSrv.getMessages();
-
-  charts = this.dashboardSrv.getCharts();
+export class DashboardComponent implements OnInit {
+  charts: any[] = [];
   chart1: any;
-  chart2: any;
-
-  stats = this.dashboardSrv.getStats();
-
   notifySubscription!: Subscription;
 
   constructor(
     private ngZone: NgZone,
-    private dashboardSrv: DashboardService,
-    private settings: SettingsService
+    private dashboardSrv: DashboardService
   ) {}
 
   ngOnInit() {
-    this.notifySubscription = this.settings.notify.subscribe(res => {
-      console.log(res);
+    this.dashboardSrv.getCharts().subscribe((data: any[]) => {
+      this.charts = data;
+      this.initChart(); // Call initChart() here
     });
   }
 
-  ngAfterViewInit() {
-    this.ngZone.runOutsideAngular(() => this.initChart());
-  }
 
-  ngOnDestroy() {
-    if (this.chart1) {
-      this.chart1?.destroy();
-    }
-    if (this.chart2) {
-      this.chart2?.destroy();
-    }
-
-    this.notifySubscription.unsubscribe();
-  }
 
   initChart() {
-    this.chart1 = new ApexCharts(document.querySelector('#chart1'), this.charts[0]);
-    this.chart1?.render();
-    this.chart2 = new ApexCharts(document.querySelector('#chart2'), this.charts[1]);
-    this.chart2?.render();
+    if (this.charts.length > 0) {
+      this.chart1 = new ApexCharts(document.querySelector('#chart1'), this.charts[0]);
+      this.chart1.render();
+    }
   }
+
+
 }
